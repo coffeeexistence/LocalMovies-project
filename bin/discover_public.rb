ab49031307dc25b2a0e5a591c9494c5f227b593b
John Anthony Rivera <askinganthony@gmail.com>
2016-01-08 17:49:59 -0800

    Application class created, Theater class created - both are barebones but functional

--- bin/discover_public.rb
+++ bin/discover_public.rb
@@ -2,5 +2,6 @@ puts "Please enter your street and zip code"
 street_zip=gets.chomp.strip
 fandango_url="http://www.fandango.com/#{zip}_movietimes"
 
+Application.set_location_interactive
 
 #https://maps.googleapis.com/maps/api/distancematrix/json?origins=39951+Via+Espana+92562&destinations=Murrieta
--- lib/Geo.rb
+++ lib/Geo.rb
@@ -1,38 +1,69 @@
 require 'json'
 require 'excon'
 require 'pry'
-class Geo
-	attr_accessor :origin
-	def initialize(origin)
-		@origin=origin
-	end
-	#https://maps.googleapis.com/maps/api/distancematrix/json?origins=39951+Via+Espana+92562&destinations=Murrieta
-	def return_data(destination)
+class Application
+	@@user_location=""
+	def self.user_location
+    	@@user_location
+  	end
+  	def self.set_location_interactive
+  		puts "Please enter your street address & zip code :)"
+		location=gets.chomp.strip
+		test_destination="San Diego, CA"
+		location_class=Maps_Distance_API.new(location, test_destination)
+		if location_class.parameters_valid? 
+			@@user_location=location_class.origin_result_full
+			puts "Location has been set to #{location_class.origin_result_full}"
+		else
+			puts "Unfortunately, that location didn't work. I can't handle that"
+			raise 'farewell cruel world'
+		end
+  	end
+end
 
-	end
+class Scrape
 
-	def time_to_location(location)
+end
 
+class Theater
+	@@all=[]
+	attr_accessor :name, :address, :movies, :location
+	def initialize(name, address)
+		@name=name
+		@address=address
+		location_class = Maps_Distance_API.new(Application.user_location, address)
+		if location_class.parameters_valid? 
+			@location=location_class
+			puts "Theater '#{@name}' At #{location_class.destination_result_full}"
+		else
+			puts "Unfortunately, that location didn't work. I can't handle that"
+			raise 'farewell cruel world'
+		end
+		self.class.all << self
 	end
+	def self.all
+    	@@all
+  	end
 end
 
 class Maps_Distance_API
-	attr_accessor :origin, :destination, :hash
+	attr_reader :origin, :destination, :hash
+
 
 
 	def initialize(origin, destination)
 		@origin=parsed(origin)
 		@destination=parsed(destination)
 		@hash=fetch_json_hash
-
+		#raise 'Maps Class had an error - invalid parameters' if !parameters_valid?
 	end
 
 	def parsed(location_str)
 		location_str.gsub(" ", "+")
 	end
 
-	def valid_parameters?
-
+	def parameters_valid?
+		@hash['status']=="OK" && @hash['rows'][0]['elements'][0]['status']=="OK"
 	end
 
 
@@ -79,5 +110,7 @@ class Maps_Distance_API
 	end
 
 end
+
+
 
 binding.pry
