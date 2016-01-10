93d439d4c09933db3a2ad00d778a1a07e9cc616e
John Anthony Rivera <askinganthony@gmail.com>
2016-01-09 23:55:51 -0800

    Now lists local movie theaters & movies

--- bin/discover_public.rb
+++ bin/discover_public.rb
@@ -1,6 +1,16 @@
 #!/usr/bin/env ruby
 require_relative "../lib/Geo.rb"
+
+
+
 #Application.set_location_interactive
 Application.set_location_interactive()
 Scrape.add_fandango_theaters
-#binding.pry
+puts ""
+puts "Theaters near you:"
+Theater.all.each do |theater| 
+	Textify.format("	"+theater.name, "|  "+theater.location.distance_miles_text+" away", 40)
+end
+puts ""
+puts "Movies Available:"
+Movie.all.each{|movie| puts "	"+movie.name}
--- lib/Geo.rb
+++ lib/Geo.rb
@@ -19,7 +19,7 @@ class Application
   			@@user_location=location_class.origin_result_full
   			return "Location forced to #{location_class.origin_result_full}"
   		end
-  		puts "Please enter your street address & zip code(mandatory) :)"
+  		puts "Please enter your street address & zip code :)"
 		location=gets.chomp.strip
 		
 		location_class=Maps_Distance_API.new(location, test_destination)
@@ -55,49 +55,90 @@ class Scrape
     end
 
 
-    def self.movies_and_showtimes_hash(theater)
-    	movie_sections = theater.css("showtimes-movie-container")
-    	hash = {}
+    def self.generate_movies_and_showtimes(theater_page, theater_class)
+    	movie_sections = theater_page.css(".showtimes-movie-container")
+
     	movie_sections.each do |movie_section|
+
     		showtime_buttons = movie_section.css(".btn-ticket")
     		showtimes = showtime_buttons.map{|showtime| showtime.css("time").text}
     		movie_title = movie_section.css(".showtimes-movie-title").text
-    		#binding.pry
-    		#movie_class = Movie.create_new_or_return_existing()
-
+    		movie_class = Movie.new_or_return_existing(movie_title)
+    		#puts "Tried to make movie: #{movie_title}"
+    		theater_class.add_movie_showtimes_arr(movie_class, showtimes)
     	end
 
     end
 
     def self.add_fandango_theaters
-    	self.get_fandango_theater_arr.each do |theater| 
-    		theater_class = self.generate_theater(theater)
-    		movies_showtimes = self.movies_and_showtimes_hash(theater)
+    	self.get_fandango_theater_arr.each do |theater_page| 
+    		theater_class = self.generate_theater(theater_page)
+    		self.generate_movies_and_showtimes(theater_page, theater_class)
 
     	end
     end
 end
 
 class Movie
+	@@all=[]
+	attr_accessor :theaters, :name, :description, :rating
+	def initialize(name)
+		@name=name
+		@theaters=[]
+		self.class.all << self
+	end
+	def showtimes
+		showtimes_hash={}
+		self.theaters.each do |theater|
+			showtimes_hash[theater.name]=theater.showtimes(self)
+		end
+	end
 
+	def self.new_or_return_existing(name)
+		search = self.all.find{|existing_movie| existing_movie.name==name}
+		return search if search
+		self.new(name)
+	end
+
+	def self.all
+		@@all
+	end
+	def self.all=(all)
+		@@all=all
+	end
 end
 
 class Theater
 	@@all=[]
-	attr_accessor :name, :address, :movies, :location
+	attr_accessor :name, :address, :movies, :location, :movie_showtimes_hash
 	def initialize(name, address)
 		@name=name
 		@address=address
+		@movies=[]
+		@movie_showtimes_hash={}
 		location_class = Maps_Distance_API.new(Application.user_location, address)
 		if location_class.parameters_valid? 
 			@location=location_class
-			puts "Theater '#{@name}' At #{location_class.destination_result_full}"
+			#puts "Theater '#{@name}' At #{location_class.destination_result_full}"
 		else
 			puts "Unfortunately, that location didn't work. I can't handle that"
 			raise 'farewell cruel world'
 		end
 		self.class.all << self
 	end
+
+	def showtimes(movie_class)
+		@movie_showtimes_hash[movie_class.name]
+	end
+
+	def add_movie_showtimes_arr(movie_class, array)
+		@movie_showtimes_hash[movie_class.name]=array
+	end
+
+	def add_movie(movie)
+		@moves << movie
+	end
+
 	def self.all
     	@@all
   	end
@@ -167,6 +208,20 @@ class Maps_Distance_API
 
 	def duration_minutes_text
 		"#{duration_minutes} minutes"
+	end
+
+end
+
+
+
+class Textify
+	def self.format(entry_1, entry_2, fixed_distance)
+		padding_length=fixed_distance-entry_1.length
+		padding=""
+		padding_length.times do 
+			padding << " "
+		end
+		puts entry_1+padding+entry_2
 	end
 
 end
